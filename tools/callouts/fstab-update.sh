645f50a2b9169db3d1ed1a8e3de58154ff4b25ba
Joe Shaw <joeshaw@novell.com>
2004-05-18 21:14:45 +0000

    Use /media as long as it's there and /mnt doesn't exist. Otherwise fallback     to /mnt. Should happily work on all distros. Patch from Sjoerd Simons     <sjoerd@luon.net>.

--- tools/callouts/fstab-update.sh
+++ tools/callouts/fstab-update.sh
@@ -9,10 +9,6 @@
 # guarantees for safety of this script.  If you want to try it
 # out, you should make a backup of your existing /etc/fstab
 # file, and you shouldn't use this on any production system!
-#
-# If you are running SUSE (or another FHS 2.3-compliant OS)
-# you should probably change the MEDIAROOT from "/mnt" to
-# "/media"
 
 if test "$HAL_PROP_BLOCK_DEVICE" = ""; then
     exit 0
@@ -36,7 +32,11 @@ fi
 # NOTE: We could use HAL_PROP_BLOCK_VOLUME_LABEL (which may or may not be 
 # available) but that would be a bad idea since it won't work for two
 # volumes with the same label
-MEDIAROOT="/mnt"
+MEDIAROOT="/media"
+if [ -d /mnt -a ! -d /media ]; then
+    MEDIAROOT="/mnt"
+fi
+
 MOUNTPOINT="$MEDIAROOT/hal/disk-$HAL_PROP_BLOCK_MAJOR-$HAL_PROP_BLOCK_MINOR-"
 
 have_lock=false
