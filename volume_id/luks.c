ee4a8e735b7fd5d8a0f679f4a2ca3907c0238e27
Kay Sievers <kay.sievers@vrfy.org>
2005-03-04 00:42:00 +0000

    Remove unneeded stuff. Version v37. Use new UUID_DCE_UNPARSED format for volume_id_set_uuid call. Add UUID_DCE_UNPARSED format. Add UUID_DCE_UNPARSED format. Only #include "luks.h" once.

--- volume_id/luks.c
+++ volume_id/luks.c
@@ -40,58 +40,47 @@
 #include "logging.h"
 #include "luks.h"
 
-/* from cryptsetup-luks internal.h */
-#define SECTOR_SHIFT            9
-#define SECTOR_SIZE             (1 << SECTOR_SHIFT)
+#define SECTOR_SHIFT			9
+#define SECTOR_SIZE			(1 << SECTOR_SHIFT)
 
-/* from cryptsetup-luks luks.h */
-#define LUKS_CIPHERNAME_L 32
-#define LUKS_CIPHERMODE_L 32
-#define LUKS_HASHSPEC_L 32
-#define LUKS_DIGESTSIZE 20 /* since SHA1 */
-#define LUKS_SALTSIZE 32
-#define LUKS_NUMKEYS 8
+#define LUKS_CIPHERNAME_L		32
+#define LUKS_CIPHERMODE_L		32
+#define LUKS_HASHSPEC_L			32
+#define LUKS_DIGESTSIZE			20
+#define LUKS_SALTSIZE			32
+#define LUKS_NUMKEYS			8
 
-/* from cryptsetup-luks luks.h */
 const unsigned char LUKS_MAGIC[] = {'L','U','K','S', 0xba, 0xbe};
 #define LUKS_MAGIC_L 6
-
-/* from cryptsetup-luks luks.h */
 #define LUKS_PHDR_SIZE (sizeof(struct luks_phdr)/SECTOR_SIZE+1)
-
-/* from cryptsetup-luks luks.h */
 #define UUID_STRING_L 40
+
+struct luks_phdr {
+	char		magic[LUKS_MAGIC_L];
+	uint16_t	version;
+	char		cipherName[LUKS_CIPHERNAME_L];
+	char		cipherMode[LUKS_CIPHERMODE_L];
+	char		hashSpec[LUKS_HASHSPEC_L];
+	uint32_t	payloadOffset;
+	uint32_t	keyBytes;
+	char		mkDigest[LUKS_DIGESTSIZE];
+	char		mkDigestSalt[LUKS_SALTSIZE];
+	uint32_t	mkDigestIterations;
+	char		uuid[UUID_STRING_L];
+	struct {
+		uint32_t active;
+		uint32_t	passwordIterations;
+		char		passwordSalt[LUKS_SALTSIZE];
+		uint32_t	keyMaterialOffset;
+		uint32_t	stripes;
+	} keyblock[LUKS_NUMKEYS];
+};
 
 int volume_id_probe_luks(struct volume_id *id, __u64 off)
 {
-	/* from cryptsetup-luks luks.h */
-	struct luks_phdr {
-		char            magic[LUKS_MAGIC_L];
-		uint16_t        version;
-		char            cipherName[LUKS_CIPHERNAME_L];
-		char            cipherMode[LUKS_CIPHERMODE_L];
-		char            hashSpec[LUKS_HASHSPEC_L];
-		uint32_t        payloadOffset;
-		uint32_t        keyBytes;
-		char            mkDigest[LUKS_DIGESTSIZE];
-		char            mkDigestSalt[LUKS_SALTSIZE];
-		uint32_t        mkDigestIterations;
-		char            uuid[UUID_STRING_L];
-		struct {
-			uint32_t active;
-
-			/* parameters used for password processing */
-			uint32_t passwordIterations;
-			char     passwordSalt[LUKS_SALTSIZE];
-
-			/* parameters used for AF store/load */
-			uint32_t keyMaterialOffset;
-			uint32_t stripes;
-		} keyblock[LUKS_NUMKEYS];
-	} *header;
+	struct luks_phdr *header;
 
 	header = (struct luks_phdr*) volume_id_get_buffer(id, off, LUKS_PHDR_SIZE);
-
 	if (header == NULL)
 		return -1;
 
@@ -99,7 +88,8 @@ int volume_id_probe_luks(struct volume_id *id, __u64 off)
 		return -1;
 
 	volume_id_set_usage(id, VOLUME_ID_CRYPTO);
-	volume_id_set_uuid(id, header->uuid, UUID_DCE);
+	volume_id_set_uuid(id, header->uuid, UUID_DCE_STRING);
+
 	id->type = "crypto_LUKS";
 
 	return 0;
