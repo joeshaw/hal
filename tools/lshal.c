2901c216a1d5867ac997e8c7805a3e530201fe71
Kay Sievers <kay.sievers@vrfy.org>
2006-07-14 17:46:57 +0000

    Replace dbus_connection_disconnect() with dbus_connection_close() (D-BUS     api rename)

--- tools/lshal.c
+++ tools/lshal.c
@@ -705,7 +705,7 @@ main (int argc, char *argv[])
 	libhal_ctx_shutdown (hal_ctx, &error);
 	libhal_ctx_free (hal_ctx);
 
-	dbus_connection_disconnect (conn);
+	dbus_connection_close (conn);
 	dbus_connection_unref (conn);
 
 	if (show_device)
