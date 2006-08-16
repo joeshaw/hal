189b452798f9441dd59d90d806b417f1a7725006
Danny Kukawka <danny.kukawka@web.de>
2006-08-16 18:48:30 +0200

    add more scsi.type mapping and add property info to spec
    Added missing scsi.type mappings which are e.g. needed to identify
    scsi printer/scanner or media changer/raid devices. Added also the
    property information about scsi.type to the spec.
    
    Mapping of kernel types is:
     TYPE_DISK, TYPE_MOD, TYPE_RBC --> disk
     TYPE_TAPE                     --> tape
     TYPE_PROCESSOR                --> processor
     TYPE_WORM, TYPE_ROM           --> cdrom
     TYPE_SCANNER                  --> scanner
     TYPE_MEDIUM_CHANGER           --> medium_changer
     TYPE_COMM                     --> comm
     TYPE_RAID                     --> raid

--- hald/linux2/physdev.c
+++ hald/linux2/physdev.c
@@ -674,24 +674,39 @@ scsi_add (const gchar *sysfs_path, HalDevice *parent)
 	hal_util_get_int_from_file (sysfs_path, "type", &type, 0);
 	HAL_INFO (("%s/type -> %d (-> scsi.type)", sysfs_path, type));
 	switch (type) {
-	case 0:
-		/* Disk */
-	case 14:
-		/* TYPE_RBC (Reduced Block Commands)
-		 * Simple Direct Access Device, set it to disk
-		 * (some Firewire Disks use it)
-		 */
+	case 0:  /* TYPE_DISK (disk) */
+	case 7:  /* TYPE_MOD (Magneto-optical disk) */
+	case 14: /* TYPE_RBC (Reduced Block Commands)
+		  * Simple Direct Access Device, set it to disk
+		  * (some Firewire Disks use it)
+		  */
 		hal_device_property_set_string (d, "scsi.type", "disk");
 		break;
-	case 1:
-		/* Tape */
+	case 1: /* TYPE_TAPE (Tape) */
 		hal_device_property_set_string (d, "scsi.type", "tape");
 		break;
-	case 4:
-		/* WORM */
-	case 5:
-		/* CD-ROM */
+	case 2:
+		/* TYPE_PRINTER (Tape) */
+		hal_device_property_set_string (d, "scsi.type", "printer");
+		break;
+	case 3:  /* TYPE_PROCESSOR */
+		hal_device_property_set_string (d, "scsi.type", "processor");
+		break;
+	case 4: /* TYPE_WORM */
+	case 5: /* TYPE_ROM (CD-ROM) */
 		hal_device_property_set_string (d, "scsi.type", "cdrom");
+		break;
+	case 6: /* TYPE_SCANNER */
+		hal_device_property_set_string (d, "scsi.type", "scanner");
+		break;
+	case 8: /* TYPE_MEDIUM_CHANGER */
+		hal_device_property_set_string (d, "scsi.type", "medium_changer");
+		break;
+	case 9: /* TYPE_COMM */
+		hal_device_property_set_string (d, "scsi.type", "comm");
+		break;
+	case 12: /* TYPE_RAID */
+		hal_device_property_set_string (d, "scsi.type", "raid");
 		break;
 	default:
 		hal_device_property_set_string (d, "scsi.type", "unknown");
