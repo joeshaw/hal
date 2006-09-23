499e981a3055ef8bf09ef7b2737721a275698d09
David Zeuthen <davidz@redhat.com>
2006-09-23 19:15:30 -0400

    make cpufreq support optional
    This is part of a larger effort to make HAL better support embedded systems.

--- hald/valgrind-hald.sh
+++ hald/valgrind-hald.sh
@@ -2,7 +2,11 @@
 
 export HALD_RUNNER_PATH=`pwd`/linux:`pwd`/linux/probing:`pwd`/linux/addons:`pwd`/.:`pwd`/../tools:`pwd`/../tools/linux
 export PATH=`pwd`/../hald-runner:$PATH
-export HAL_FDI_SOURCE_PREPROBE=../fdi/preprobe
-export HAL_FDI_SOURCE_INFORMATION=../fdi/information
-export HAL_FDI_SOURCE_POLICY=../fdi/policy
+
+rm -rf .local-fdi
+make -C ../fdi install DESTDIR=`pwd`/.local-fdi prefix=/
+export HAL_FDI_SOURCE_PREPROBE=.local-fdi/share/hal/fdi/preprobe
+export HAL_FDI_SOURCE_INFORMATION=.local-fdi/share/hal/fdi/information
+export HAL_FDI_SOURCE_POLICY=.local-fdi/share/hal/fdi/policy
+
 valgrind --num-callers=20 --show-reachable=yes --leak-check=yes --tool=memcheck ./hald --daemon=no --verbose=yes
