38ad04e7c72a1bd4837583b64fafe1ee0a3d2b7b
Danny Kukawka <danny.kukawka@web.de>
2008-03-14 13:26:53 +0100

    fixed compiler warning for 64bit related to usage of GQuark
    Fixed compiler warning for 64bit related to usage of cast to GQuark.

--- hald/device.c
+++ hald/device.c
@@ -871,7 +871,7 @@ hdpfe (gpointer key,
 {
 	hdpfe_ud_t *c;
 	HalProperty *prop;
-	const gchar *key_string = g_quark_to_string ((GQuark) key);
+	const gchar *key_string = g_quark_to_string ((GQuark) GPOINTER_TO_UINT(key));
 
 	c = (hdpfe_ud_t *) user_data;
 	prop = (HalProperty *) value;
