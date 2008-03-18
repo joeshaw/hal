8f5e5cf7a77bc9489021911892ac8128797971db
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-03-18 13:00:08 -0400

    call libhal_device_addon_is_ready()
    We were not calling libhal_device_addon_is_ready like a good addon is supposed
    to.  This meant that auto-mounting HAL volumes in Nautilus would not work
    properly.

--- hald/freebsd/addons/addon-storage.c
+++ hald/freebsd/addons/addon-storage.c
@@ -202,6 +202,10 @@ main (int argc, char **argv)
     ! strcmp(driver, "cd")))) && ! strcmp(removable, "true");
   addon.had_media = hf_addon_storage_update();
 
+  if (!libhal_device_addon_is_ready(hfp_ctx, hfp_udi, &hfp_error))
+    goto end;
+  dbus_error_free(&hfp_error);
+
   connection = libhal_ctx_get_dbus_connection(hfp_ctx);
   assert(connection != NULL);
 
