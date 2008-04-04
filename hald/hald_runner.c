89591f68dd545adaf5f5564b973077c73559eb79
Danny Kukawka <danny.kukawka@web.de>
2008-04-04 13:42:07 +0200

    fixed check for return value of uname()
    Fixed check for return value of uname() as pointed out by Phi Tran
    <Phi.Tran@sun.com> on the HAL mailing list:
    
    > hald_runner.c line 513 uses uname and checks for the return value
    > equal to zero as success.  On Solaris, uname returns >= 0 on success.

--- hald/hald_runner.c
+++ hald/hald_runner.c
@@ -510,7 +510,7 @@ add_basic_env (DBusMessageIter * iter, const gchar * udi)
 	}
 #endif /* HAVE_CONKIT */
 
-	if (uname (&un) == 0) {
+	if (uname (&un) >= 0) {
 		char *sysname;
 
 		sysname = g_ascii_strdown (un.sysname, -1);
