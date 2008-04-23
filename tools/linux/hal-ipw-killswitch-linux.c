680443a4bbba7d0eb98557433615150e941bee57
Danny Kukawka <danny.kukawka@web.de>
2008-04-23 22:29:23 +0200

    cleaned up hal-ipw-killswitch-linux
    Cleaned up hal-ipw-killswitch-linux. Use fgets/strtol as in util.c of HAL
    instead of fgetc, added better error handling, use syslog to log error
    messages also if HAL is in daemon mode.

--- tools/linux/hal-ipw-killswitch-linux.c
+++ tools/linux/hal-ipw-killswitch-linux.c
@@ -21,8 +21,10 @@
  *
  **************************************************************************/
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <syslog.h>
 #include <glib.h>
 #include <stdlib.h>
 
@@ -44,25 +46,26 @@ int main(int argc,char** argv) {
 	char *path;
 	int ret = -1;
 
-	if(argc == 1) return -1;	
+	if (argc == 1) 
+		return -1;	
 
 	dbus_error_init (&error);
 	
-	if((udi = getenv ("HAL_PROP_INFO_UDI")) == NULL) return -1;
+	if ((udi = getenv ("HAL_PROP_INFO_UDI")) == NULL) return -1;
 
 	if ((hal_ctx = libhal_ctx_new ()) == NULL) {
-		fprintf (stderr, "error: libhal_ctx_new\n");
+		syslog (LOG_INFO, "error: libhal_ctx_new\n");
 		return -1;
 	}
 
 	if (!libhal_ctx_set_dbus_connection (hal_ctx, dbus_bus_get (DBUS_BUS_SYSTEM, &error))) {
-		fprintf (stderr, "error: libhal_ctx_set_dbus_connection: %s: %s\n", error.name, error.message);
+		syslog (LOG_INFO, "error: libhal_ctx_set_dbus_connection: %s: %s\n", error.name, error.message);
 		LIBHAL_FREE_DBUS_ERROR (&error);
 		return -1;
 	}
 
 	if (!libhal_ctx_init (hal_ctx, &error)) {
-		fprintf (stderr, "error: libhal_ctx_init: %s: %s\n", error.name, error.message);
+		syslog (LOG_INFO, "error: libhal_ctx_init: %s: %s\n", error.name, error.message);
 		LIBHAL_FREE_DBUS_ERROR (&error);
 		return -1;
 	}
@@ -70,60 +73,83 @@ int main(int argc,char** argv) {
 
 	parent = libhal_device_get_property_string (hal_ctx, udi, "info.parent", &error);
 	udis = libhal_manager_find_device_string_match (hal_ctx, "info.parent", parent, &num_udis, &error);
-	libhal_free_string (parent);
 	
-	if(argc==2 && strcmp("getrfkill",argv[1])==0) {
+	if( argc==2 && strcmp("getrfkill",argv[1])==0) {
 
 		for (i = 0; i < num_udis; i++) {
+			char buf[64];
 
-			if(strcmp (udis[i], udi) == 0) continue;
+			if (strcmp (udis[i], udi) == 0) 
+				continue;
+
 			iface = libhal_device_get_property_string (hal_ctx, udis[i], "net.interface", &error);
-			path = g_strdup_printf ("/sys/class/net/%s/device/rf_kill", iface);
-			if((fd=fopen (path, "r")) == NULL) return -1;
-			kill_status = fgetc (fd);
-			fclose (fd);
-			g_free (path);
+			if (iface != NULL) {
+				path = g_strdup_printf ("/sys/class/net/%s/device/rf_kill", iface);
 
-			switch(kill_status) {
-				case '0':
-					ret = 0;
-				break;
-				case '1':
-				case '2':
-				case '3':
-					ret = 1;
-				break;
+				if ((fd = fopen (path, "r")) == NULL) {
+					return -1;
+				}
+				if (fgets (buf, sizeof (buf), fd) == NULL) {
+					return -1;
+				}
+
+				errno = 0;
+				kill_status = strtol (buf, NULL, 10);
+				if (errno == 0) {
+					/* syslog (LOG_INFO, "'%s' returned %d", path, kill_status); */
+					
+					switch(kill_status) {
+						case 0:
+							ret = 0;
+							break;
+						case 1:
+						case 2:
+						case 3:
+							ret = 1;
+							break;
+						default:
+							break;
+					}
+				}
+
+				fclose (fd);
+				g_free (path);
+				libhal_free_string (iface);
 			}
-
-			libhal_free_string (iface);
 		}
-	
-		libhal_free_string_array (udis);
 	}
 
-	if(argc == 3 && strcmp ("setrfkill", argv[1]) == 0 && (atoi (argv[2]) == 0 || atoi(argv[2]) == 1)) {
+	if (argc == 3 && strcmp ("setrfkill", argv[1]) == 0 && (atoi (argv[2]) == 0 || atoi(argv[2]) == 1)) {
 
 		for (i = 0; i < num_udis; i++) {
+			if (strcmp (udis[i], udi) == 0) 
+				continue;
 
-			if(strcmp (udis[i], udi)==0) continue;
 			iface = libhal_device_get_property_string (hal_ctx, udis[i], "net.interface", &error);
-			path = g_strdup_printf ("/sys/class/net/%s/device/rf_kill", iface);
-			if((fd=fopen (path, "w")) == NULL) return -1;
-			fputc (argv[2][0], fd);
-			fclose (fd);
-			g_free (path);
-			libhal_free_string (iface);
+			if (iface != NULL) {
+				path = g_strdup_printf ("/sys/class/net/%s/device/rf_kill", iface);
+				
+				if ((fd = fopen (path, "w")) == NULL) {
+					return -1;
+				}
+
+				fputc (argv[2][0], fd);
+				fclose (fd);
+
+				g_free (path);
+				libhal_free_string (iface);
+			}
 		}
 	
-		libhal_free_string_array (udis);	
-
 		ret = 0;
 	}
 	
+	libhal_free_string (parent);
+	libhal_free_string_array (udis);
 	libhal_ctx_free (hal_ctx);
 
 	if (dbus_error_is_set (&error)) {
-		fprintf (stderr, "error: %s: %s\n", error.name, error.message);
+		syslog (LOG_INFO, "error: %s: %s\n", error.name, error.message);
 		dbus_error_free (&error);
 		return -1;
 	}
