900b43c2ec805ec496144d51f0d33d48cd860bbe
Kay Sievers <kay.sievers@vrfy.org>
2009-05-15 02:13:38 +0200

    remove broken ASCII 240 chars from C files

--- tools/hal-system-sonypic.c
+++ tools/hal-system-sonypic.c
@@ -63,7 +63,7 @@
 static int
 sonypi_get_lcd_brightness (__u8 *val)
 {
-#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
+#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
 	return FALSE;			/* FIXME implement */
 #elif sun
 	return FALSE;			/* FIXME implement */
@@ -163,7 +163,7 @@ error_set:
 static int
 sonypi_set_lcd_brightness (__u8 val)
 {
-#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
+#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
 	return FALSE;			/* FIXME implement */
 #elif sun
 	return FALSE;			/* FIXME implement */
@@ -195,7 +195,7 @@ sonypi_set_lcd_brightness (__u8 val)
 static int
 sonypi_set_bluetooth_power (int val)
 {
-#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
+#if defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
 	return FALSE;			/* FIXME implement */
 #elif sun
 	return FALSE;			/* FIXME implement */
