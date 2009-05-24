e4ac5deba9287ea6b66b7b9c1f4c4d2ee1cc69b4
Simon Munton <simon-hal@munton.demon.co.uk>
2009-05-24 18:01:51 +0200

    fixed segfault in hal-storage-mount
    Fixed segfault in hal-storage-mount and don't lose fstype in
    probe-volume.

--- hald/linux/probing/probe-volume.c
+++ hald/linux/probing/probe-volume.c
@@ -97,7 +97,7 @@ set_blkid_values (LibHalChangeSet *cs, blkid_probe pr)
 
 	if (blkid_probe_lookup_value(pr, "TYPE", &type, NULL))
 		type = "";
-	if (libhal_changeset_set_property_string (cs, "volume.fstype", type))
+	if (!libhal_changeset_set_property_string (cs, "volume.fstype", type))
 		libhal_changeset_set_property_string (cs, "volume.fstype", "");
 	HAL_DEBUG(("volume.fstype = '%s'", type));
 
