d3aeab79ada75e4a6ef245f4f915317fbcea8918
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-08-18 12:57:34 -0400

    add support for reading ufsids
    Fix a problem where duplicate volumes may get mounted by teaching hal
    how to read and check for unique ufsids.

--- hald/freebsd/probing/probe-volume.c
+++ hald/freebsd/probing/probe-volume.c
@@ -36,7 +36,12 @@
 #include <sys/disk.h>
 #include <sys/cdio.h>
 #include <sys/param.h>
+#include <sys/mount.h>
 #include <sys/types.h>
+#include <ufs/ufs/ufsmount.h>
+#include <ufs/ufs/dinode.h>
+#include <ufs/ffs/fs.h>
+#include <libufs.h>
 #include <isofs/cd9660/iso.h>
 #include <glib.h>
 #include <libvolume_id.h>
@@ -502,7 +507,8 @@ main (int argc, char **argv)
 	  hfp_cdrom_free(cdrom);
 	}
 
-      if (has_data)
+      if (has_data && vid && (! strcmp(vid->type, "iso9660") ||
+          ! strcmp(vid->type, "udf")))
         hf_probe_volume_advanced_disc_detect(fd);
     }
   else
@@ -554,6 +560,40 @@ main (int argc, char **argv)
   libhal_device_set_property_bool(hfp_ctx, hfp_udi, "volume.is_partition", is_partition, &hfp_error);
 
   libhal_device_set_property_bool(hfp_ctx, hfp_udi, "volume.ignore", has_children || is_swap, &hfp_error);
+
+  if (vid && ! strcmp (vid->type, "ufs"))
+    {
+      struct uufsd ufsdisk;
+
+      if (ufs_disk_fillout(&ufsdisk, device_file) == 0)
+        {
+	  char ufsid[64];
+	  char **ufs_devs;
+	  int num_udis;
+	  int i;
+
+	  snprintf(ufsid, sizeof(ufsid), "%08x%08x", ufsdisk.d_fs.fs_id[0], ufsdisk.d_fs.fs_id[1]);
+	  libhal_device_set_property_string(hfp_ctx, hfp_udi, "volume.freebsd.ufsid", ufsid, &hfp_error);
+	  ufs_devs = libhal_manager_find_device_string_match(hfp_ctx,
+			  				     "volume.freebsd.ufsid",
+							     ufsid,
+							     &num_udis,
+							     &hfp_error);
+	  dbus_error_free(&hfp_error);
+	  for (i = 0; i < num_udis; i++)
+            {
+              if (ufs_devs[i] != NULL)
+                {
+                  gboolean mounted;
+
+		  mounted = libhal_device_get_property_bool(hfp_ctx, ufs_devs[i], "volume.is_mounted", &hfp_error);
+		  if (mounted)
+                    libhal_device_set_property_bool(hfp_ctx, hfp_udi, "volume.ignore", TRUE, &hfp_error);
+		}
+	    }
+	  ufs_disk_close(&ufsdisk);
+	}
+    }
 
   if (has_children)
     usage = "partitiontable";
