c31d30640707aa63928aa20e067da8b19405e663
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-08-19 16:23:12 -0400

    ignore probe devices
    Ignore probeX devices when scanning for new SCSI disks.

--- hald/freebsd/hf-scsi.c
+++ hald/freebsd/hf-scsi.c
@@ -549,7 +549,7 @@ hf_scsi_probe (void)
 		break;		/* only use the first peripheral */
 
 	      match = &ccb.cdm.matches[i].result.periph_result;
-	      if ((int) match->path_id == -1 || ! strcmp(match->periph_name, "pass"))
+	      if ((int) match->path_id == -1 || ! strcmp(match->periph_name, "pass") || ! strcmp(match->periph_name, "probe"))
 		break;
 
 	      pending_devname = g_strdup_printf("%s%i", match->periph_name, match->unit_number);
