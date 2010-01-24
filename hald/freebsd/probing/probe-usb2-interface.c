4c1d80dd2a77adf3392ca7f9479abb727636b9e5
Joe Marcus Clarke <marcus@FreeBSD.org>
2010-01-23 20:32:17 -0500

    Fix the environment variable names
    Now that the property names for USB interfaces have been fixed, correct
    the environment variable names.

--- hald/freebsd/probing/probe-usb2-interface.c
+++ hald/freebsd/probing/probe-usb2-interface.c
@@ -58,11 +58,11 @@ main(int argc, char **argv)
   if (pbe == NULL)
     goto end;
 
-  busstr = getenv("HAL_PROP_USB_DEVICE_BUS_NUMBER");
+  busstr = getenv("HAL_PROP_USB_BUS_NUMBER");
   if (! busstr)
     goto end;
 
-  addrstr = getenv("HAL_PROP_USB_DEVICE_PORT_NUMBER");
+  addrstr = getenv("HAL_PROP_USB_PORT_NUMBER");
   if (! addrstr)
     goto end;
 
